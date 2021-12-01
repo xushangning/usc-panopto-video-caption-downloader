534e0d1ebc6843f56b28bc4170b2dfc5bb04b261
Shining Xu <shangnin@usc.edu>
2021-11-30 19:45:26 -0800

    Replace as many "let" with "const" as possible

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let downloadCaptionsButton = document.getElementsByClassName('action-buttons')[0]
+const downloadCaptionsButton = document.getElementsByClassName('action-buttons')[0]
     .appendChild(document.createElement('li'))
     .appendChild(document.createElement('button'));
 downloadCaptionsButton.textContent = 'Download captions';
@@ -13,25 +13,25 @@ downloadCaptionsButton.addEventListener('click', async (event) => {
 
     const ENDPOINT = 'https://uscviterbi.hosted.panopto.com/Panopto/Pages/Viewer/DeliveryInfo.aspx';
     const videoRow = document.querySelector('#detailsTable tr');
-    let postBody = new URLSearchParams([
+    const postBody = new URLSearchParams([
         ['getCaptions', 'true'],
         ['language', '0'],
         ['responseType', 'json']
     ]);
 
     postBody.set('deliveryId', videoRow.id);
-    let resp = await fetch(new Request(ENDPOINT, {
+    const resp = await fetch(new Request(ENDPOINT, {
         method: 'POST',
         body: postBody,
     }));
-    let captions = await resp.json();
+    const captions = await resp.json();
 
-    let captionArray = [];
+    const captionArray = [];
     for (const line of captions) {
         captionArray.push(line.Caption + '\n');
     }
 
-    let captionUrl = URL.createObjectURL(new Blob(captionArray, {type: 'text/plain'}));
+    const captionUrl = URL.createObjectURL(new Blob(captionArray, {type: 'text/plain'}));
     chrome.runtime.sendMessage(
         {
             type: 'download',
